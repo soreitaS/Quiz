00fd4b66086e6caba5eff2c9c5e6c2a6f9d11aaf
ibrahimince <ibr6629inc@gmail.com>
2026-02-16 22:08:22 +0300

    init

---
+++ Quiz/Quiz.Client/Program.cs
@@ -0,0 +1,6 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
